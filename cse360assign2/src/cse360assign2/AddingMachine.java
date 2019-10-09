0b561f714d8505691f862927747b9b7b721d83df
overlyoptimisticprogramer <46108795+overlyoptimisticprogramer@users.noreply.github.com>
2019-10-09 14:39:05 -0700

    Added javadoc comments.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,31 +1,54 @@
 package cse360assign2;
-/**
- * some javadoc documentation
- */
+
 public class AddingMachine {
 
+	/** 
+	 * The total value of the adding machine that is getting modified
+	 */
 	private int total;
 	
+	/** 
+	 * Initial call that sets the total value
+	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * Returns the total value of the adding machine
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/**
+	 * Adds the value to the total value of the adding machine
+	 * 
+	 * @param value	the value to be added to the adding machine
+	 */
 	public void add (int value) {
 		
 	}
-	
+
+	/**
+	 * Subtracts the value and the total value of the adding machine
+	 * 
+	 * @param value	the value to be subtracted from the adding machine
+	 */	
 	public void subtract (int value) {
 		
 	}
 		
+	/**
+	 * Returns the value of the adding machine as a string
+	 */
 	public String toString () {
 		return "";
 	}
 
+	/**
+	 * Clears the adding machine
+	 */
 	public void clear() {
 	
 	}
