0108421764cde1154bb5a3855f698adf6049f24c
overlyoptimisticprogramer <46108795+overlyoptimisticprogramer@users.noreply.github.com>
2019-10-09 15:09:06 -0700

    Added basic functionality.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,55 +1,58 @@
-package cse360assign2;
+ 
 
 public class AddingMachine {
 
-	/** 
-	 * The total value of the adding machine that is getting modified
-	 */
-	private int total;
-	
-	/** 
-	 * Initial call that sets the total value
-	 */
-	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
-	}
-	
-	/**
-	 * Returns the total value of the adding machine
-	 */
-	public int getTotal () {
-		return 0;
-	}
-	
-	/**
-	 * Adds the value to the total value of the adding machine
-	 * 
-	 * @param value	the value to be added to the adding machine
-	 */
-	public void add (int value) {
-		
-	}
+    /** 
+     * The total value of the adding machine that is getting modified
+     */
+    private int total;
+    private String history;
+    
+    /** 
+     * Initial call that sets the total value
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+        history = "";
+    }
+    
+    /**
+     * Returns the total value of the adding machine
+     */
+    public int getTotal () {
+        return total;
+    }
+    
+    /**
+     * Adds the value to the total value of the adding machine
+     * 
+     * @param value the value to be added to the adding machine
+     */
+    public void add (int value) {
+        total += value;
+    }
 
-	/**
-	 * Subtracts the value and the total value of the adding machine
-	 * 
-	 * @param value	the value to be subtracted from the adding machine
-	 */	
-	public void subtract (int value) {
-		
-	}
-		
-	/**
-	 * Returns the value of the adding machine as a string
-	 */
-	public String toString () {
-		return "";
-	}
+    /**
+     * Subtracts the value and the total value of the adding machine
+     * 
+     * @param value the value to be subtracted from the adding machine
+     */ 
+    public void subtract (int value) {
+        
+        total -= value;
+    }
+        
+    /**
+     * Returns the value of the adding machine as a string
+     */
+    public String toString () {
+        return history + Integer.toString( total );
+    }
 
-	/**
-	 * Clears the adding machine
-	 */
-	public void clear() {
-	
-	}
+    /**
+     * Clears the adding machine
+     */
+    public void clear() {
+        history = "";
+    }
 }
